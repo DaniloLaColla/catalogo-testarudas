b0aca9ffb97f0695db6cbd4276beecc97ce5c9ee
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2025-11-30 17:44:12 -0300

    fix numero

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -49,7 +49,7 @@ const ProductDetail = () => {
                         </p>
                     </div>
                     <a
-                        href={`https://wa.me/549298465991?text=Hola! Me interesa el producto "${product.name}" que vi en el catálogo: ${window.location.href}`}
+                        href={`https://wa.me/5492984654991?text=Hola! Me interesa el producto "${product.name}" que vi en el catálogo: ${window.location.href}`}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="btn btn-primary btn-whatsapp"
